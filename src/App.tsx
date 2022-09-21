226f552c09a5e5ce3c66d1573949d3e13c212cea
Gance123 <110610675+Gance123@users.noreply.github.com>
2022-09-21 13:08:34 +0900

    proceed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 import "./styles.css";
 
 export default function App() {
+  // ロジック部分を切り抜いて新たに作成したhooksから、必要な引数と関数を呼び出す
+  //・・・ロジック部分とは、例えばuseStateでいうset~~~の部分
+  const { getUsers, userProfile, loading, error } = useAllUsers();
+
+  const onClickFechUser = () => {
+    getUsers();
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFechUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading....</p>
+      ) : (
+        <>
+          {userProfile.map((user) => (
+            <UserCard key={user.id} user={user} />
+            //user = user.data
+          ))}
+        </>
+      )}
     </div>
   );
 }
